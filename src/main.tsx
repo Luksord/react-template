b243fe413fa4aeae5fe150048b0c20c1680f1efa
Łukasz Wątorski <watorski.info@gmail.com>
2024-10-26 23:48:59 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import App from 'components/App';
 import './global.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -9,31 +11,11 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <BrowserRouter>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
-// import { PersistGate } from "redux-persist/integration/react";
-// import { BrowserRouter } from "react-router-dom";
-// import { HelmetProvider } from "react-helmet-async";
-// import { persistedStore, store } from "redux/store";
-// import { App } from "components/App";
-// import "./index.css";
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistedStore}>
-//         <BrowserRouter>
-//           <HelmetProvider>
-//             <App />
-//           </HelmetProvider>
-//         </BrowserRouter>
-//       </PersistGate>
-//     </Provider>
-//   </React.StrictMode>
-// );
